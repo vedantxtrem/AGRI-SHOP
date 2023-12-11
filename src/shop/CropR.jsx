ebf4917f3fa4903b3bd7ac62d8387f1a44ffebba
Vedant Sahu <vedant@pop-os.localdomain>
2023-12-11 22:56:50 +0530

    changes

--- src/shop/CropR.jsx
+++ src/shop/CropR.jsx
@@ -97,33 +97,100 @@ function CropRecommendationForm() {
                         </p>
                     </div>
                 </div>
-                <div className='w-full p-5 flex justify-center' >
-                    <table className="table-auto rounded-xl border-collapse w-full">
-                        <thead>
-                            <tr className="bg-green-200">
-                                <th className="border-2 border-gray-500 px-4 py-2">Column 1</th>
-                                <th className="border-2 border-gray-500 px-4 py-2">Column 2</th>
-                                <th className="border-2 border-gray-500 px-4 py-2">Column 3</th>
+                <div class="flex justify-center  relative  overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-9/12 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Product name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Color
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Category
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Price
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Action
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className="bg-green-100">
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 1</td>
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 2</td>
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 3</td>
+                            <tr class="odd:bg-white odd:dark:bg-green-900 even:bg-gray-50 even:dark:bg-green-800 border-b dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td class="px-6 py-4">
+                                    Silver
+                                </td>
+                                <td class="px-6 py-4">
+                                    Laptop
+                                </td>
+                                <td class="px-6 py-4">
+                                    $2999
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
                             </tr>
-                            <tr className="bg-green-200">
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 4</td>
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 5</td>
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 6</td>
+                            <tr class="odd:bg-white odd:dark:bg-green-900 even:bg-gray-50 even:dark:bg-green-800 border-b dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Microsoft Surface Pro
+                                </th>
+                                <td class="px-6 py-4">
+                                    White
+                                </td>
+                                <td class="px-6 py-4">
+                                    Laptop PC
+                                </td>
+                                <td class="px-6 py-4">
+                                    $1999
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
                             </tr>
-                            <tr className="bg-green-100">
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 7</td>
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 8</td>
-                                <td className="border-2 border-gray-500 px-4 py-2">Data 9</td>
+                            
+                            <tr class="odd:bg-white odd:dark:bg-green-900 even:bg-gray-50 even:dark:bg-green-800 border-b dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Google Pixel Phone
+                                </th>
+                                <td class="px-6 py-4">
+                                    Gray
+                                </td>
+                                <td class="px-6 py-4">
+                                    Phone
+                                </td>
+                                <td class="px-6 py-4">
+                                    $799
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
                             </tr>
+                            <tr class="odd:bg-white odd:dark:bg-green-900 even:bg-gray-50 even:dark:bg-green-800 border-b dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Google Pixel Phone
+                                </th>
+                                <td class="px-6 py-4">
+                                    Gray
+                                </td>
+                                <td class="px-6 py-4">
+                                    Phone
+                                </td>
+                                <td class="px-6 py-4">
+                                    $799
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
+                            </tr>
+                            
                         </tbody>
-                    </table>    
+                    </table>
                 </div>
 
             </div>
