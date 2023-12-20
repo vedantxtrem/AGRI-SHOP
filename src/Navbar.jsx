5196a38aeb541201b84e85887dfda49393b2ac31
Vedant Sahu <vedant@pop-os.localdomain>
2023-12-20 21:49:55 +0530

    nav update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './shop/assets/agrihelplogo.png';
 import { useState } from 'react';
 
 function Navbar() {
@@ -14,52 +15,94 @@ function Navbar() {
   };
   return (
     <section>
-      <header class=" bg-green-500 m-2 rounded-full  absolute inset-x-0 top-0 z-50">
+      <header class="bg-green-500 w-full h-16 absolute inset-x-0 top-0 z-50 flex justify-center items-center">
 
-        <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global drop-shadow-2xl">
-          <div class="flex lg:flex-1">
+        <nav class="w-full flex items-center justify-between p-6 lg:px-8" aria-label="Global drop-shadow-2xl">
+          <div class="w-[10%] flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5 flex ">
               <span class="sr-only">Vedant</span>
-              <img class="h-8 w-auto" src="" alt="" />
+              <img class="h-14 w-auto" src={logo} alt="" />
             </a>
           </div>
-          <div class="flex lg:hidden">
-            <button type="button" onClick={handleClickToOpen}
-              class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-              <span class="sr-only">Open main menu</span>
-              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-            </button>
-          </div>
-          <div class="hidden lg:flex lg:gap-x-6 text-white">
-            <Link href="/Weather"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Weather</Link>
-            <Link href="/Croprecomendation"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Crop Recomendation</Link>
-            <Link href="#"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Pest Information</Link>
-            <Link href="#"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Fertilizer Recomendation</Link>
-            <Link href="#"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Shop</Link>
-            <Link href="#"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Chat</Link>
-            <Link href="#"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Govt.Scheme</Link>
-            <Link href="#"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">Feedback</Link>
-            <Link href="#"
-              class="text-xm font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:border-b-4 hover:border-green-700 text-center">About Us</Link>
+
+          <div class="w-[70%] hidden lg:flex lg:justify-center lg:gap-x-6 text-white">
+            <ul class="hidden lg:flex lg:gap-x-6 text-white">
+              <li
+                class="text-xl font-bold hover:text-green-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                Home
+              </li>
+              <li onClick={handleClickToOpen}
+                class="flex justify-center items-center text-xl font-bold hover:text-green-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ">
+                Feature's
+                <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                  fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2" d="m1 1 4 4 4-4" />
+                </svg>
+              </li>
+              <li
+                class="text-xl font-bold hover:text-green-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                Agri-Shop
+              </li>
+              <li
+                class="text-xl font-bold hover:text-green-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                Contact
+              </li>
+              <li
+                class="text-xl font-bold hover:text-green-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                About
+              </li>
+            </ul>
 
           </div>
-          <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#"
-              class="text-xl font-semibold leading-6 px-1 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 text-white">Log
-              in <span aria-hidden="true">&rarr;</span></a>
+          <div class="w-[20%] flex items-center lg:justify-around">
+
+            <div
+              class="hidden absolute inset-y-0 right-0 lg:flex lg:items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <button type="button"
+                class="relative flex justify-center items-center bg-white border focus:outline-none shadow text-gray-600 rounded focus:ring ring-gray-200 group">
+                <p class="px-4">Language</p>
+                <span class="border-l p-2 hover:bg-gray-100">
+                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewbox="0 0 24 24"
+                    xmlns="https://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 9l-7 7-7-7"></path>
+                  </svg>
+                </span>
+
+                <div
+                  class="absolute hidden group-focus:block top-full min-w-full w-max bg-white shadow-md mt-1 rounded">
+                  <ul class="text-left border rounded">
+                    <li class="px-4 py-1 hover:bg-gray-100 border-b">English </li>
+                    <li class="px-4 py-1 hover:bg-gray-100 border-b">Hindi/हिंदी</li>
+                    <li class="px-4 py-1 hover:bg-gray-100 border-b">Marathi/मराठी</li>
+                    <li class="px-4 py-1 hover:bg-gray-100 border-b">Tamil/தமிழ்</li>
+                    <li class="px-4 py-1 hover:bg-gray-100 border-b">Telugu/తెలుగు</li>
+                    <li class="px-4 py-1 hover:bg-gray-100 border-b">Punjabi/ਪੰਜਾਬੀ</li>
+                    <li class="px-4 py-1 hover:bg-gray-100 border-b">Assamese/অসমীয়া</li>
+                  </ul>
+                </div>
+              </button>
+            </div>
+
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a href="#"
+                class="text-xl font-bold text-white  hover:text-green-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Log
+                in <span aria-hidden="true">&rarr;</span></a>
+            </div>
+            <div class="flex fixed right-6 lg:hidden">
+              <button type="button" onClick={handleClickToOpen}
+                class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                <span class="sr-only">Open main menu</span>
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                  stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+              </button>
+            </div>
           </div>
+
         </nav>
 
 
@@ -70,14 +113,14 @@ function Navbar() {
               class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-green-500 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div class="flex items-center justify-between">
                 <a href="#" class="-m-1.5 p-1.5">
-                  <span class="sr-only">AGRI-HELP</span>
-                  <img class="h-8 w-auto" src="" alt="" />
+                  <span class="sr-only text-white">AGRI-HELP</span>
+                  <img class="h-8 w-auto" src={logo} alt="" />
                 </a>
 
                 <button onClick={handleToClose} type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
                   <span class="sr-only">Close menu</span>
-                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                    aria-hidden="true">
+                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>
                 </button>
@@ -89,29 +132,38 @@ function Navbar() {
 
                   <div class="space-y-2 py-6">
                     <Link to="/Weather"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Weather</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Weather</Link>
                     <Link to="/Croprecomendation"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Crop Recommendation</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Crop Recommendation</Link>
                     <Link to="/Pestprediction"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Pest Information</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Pest Information</Link>
                     <Link to="#"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Fertilizer Recommendation</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Fertilizer Recommendation</Link>
                     <Link to="/Shop"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Shop</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Shop</Link>
                     <Link to="/Chat"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Chat</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Chat</Link>
                     <Link to="#"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Feedback</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Feedback</Link>
                     <Link to="#"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">About</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      About</Link>
 
                   </div>
-
                   <div class="py-6">
                     <Link to="#"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Log in</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Log in</Link>
                     <Link to="#"
-                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-white ">Sign Up</Link>
+                      class="block h-14 text-center rounded-lg px-3 pt-4  font-semibold leading-7  text-gray-900 hover:bg-green-300 ">
+                      Sign Up</Link>
                   </div>
 
                 </div>
@@ -119,9 +171,11 @@ function Navbar() {
             </div>
           </div>
         </dialog>
+
       </header>
-     
+
     </section>
+
   );
 }
 export default Navbar;
