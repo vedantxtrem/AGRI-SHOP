a0996752d584d305ef1ee496f001cdeff174768c
Vedant Sahu <vedant@pop-os.localdomain>
2023-12-23 19:19:20 +0530

    :pray

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 
-function Body(){
-    return(
-        <div class="h-screen w-full sm:textx-xm md:text-2xl flex justify-center items-center">
-        <div className='flex-col text-center'>
-          <h2>Revolutionizing Farming With Innovative Technology Solutions</h2>
-          <h2>A Helping Hand For Farmers  &#127995</h2>
-        </div>
+function Body() {
+  return (
+    <div class="h-screen w-full sm:textx-xm md:text-2xl flex justify-center items-center ">
+
+      <div className=' fixed top-[50%] left-[25%]  text-center z-40 font-bold text-white text-3xl font-serif '>
+        <h2 className="drop-shadow-[0_10px_5px_#1e1e1e]">Revolutionizing Farming With Innovative Technology Solutions</h2>
+        <h2 className="drop-shadow-[0_10px_5px_#1e1e1e]">A Helping Hand For Farmers 🙏 </h2>
       </div>
-    );
+
+      <iframe src="https://momento360.com/e/u/2790c82f79b64648a703c06fa2bf1c04?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true" className="h-full z-0 w-full ">
+
+      </iframe>
+      
+    </div>
+
+  );
 }
 export default Body;
